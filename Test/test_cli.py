96ac41393296b23f80031c0cf1181c9f2593e627
Candy Susan <candysusan55@gmail.com>
2018-09-26 14:04:24 +0300

    Add files

---
+++ Test/test_cli.py
@@ -0,0 +1,27 @@
+import unittest
+from api.cli import NewsApi
+
+
+class TestNewsApi(unittest.TestCase):
+    def setUp(self):
+        self.news_api = NewsApi()
+        self.api_key = None
+
+    def test_api_key_is_available(self):
+        with(self.news_api.get_api_key()):
+            self.assertRaises(ValueError)
+
+    def test_api_key_is_successfull(self):
+        self.assertEqual(self.news_api.get_api_key(),
+                         'NEWS_API_KEY')
+
+    def test_number_of_headlines_is_equal_to_10(self):
+        self.assertEqual(self.news_api.display_headline(), 10)
+
+    def test_user_chooses_news_channel(self):
+        self.assertIn(self.news_api.get_news_channel, ('cnbc-news', 'cnbc',
+                                                       'bbc-news', 'cnn'))
+
+
+if __name__ == '__main__':
+    unittest.main()
